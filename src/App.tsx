982a26888cbcf5197c9752de910df41682643498
vchho <12765624+vchho@users.noreply.github.com>
2023-06-04 15:43:20 -0700

    Basic chip add and remove created. Need to figure out key issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,64 @@
-import { useAtom } from "jotai";
-import { folderAtom, folderAtom2 } from "./store/folderAtom";
 import { StandardChip, updatedStandardChips } from "./utils/chips";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const megaChip = 5;
+const gigaChip = 1;
 
 function App() {
-  const [folder, setFolderItem] = useAtom(folderAtom);
-  const [folder2, setFolderItem2] = useAtom(folderAtom2);
-  const [count, setCount] = useState<any>({});
-  const [myMap, setMap] = useState(new Map());
+  const [folder, setFolder] = useState<StandardChip[]>([]);
+  const [folderTrack, setFolder2] = useState<any[]>([]);
 
-  const getTotalCount = () => {
-    let totalCount = 0;
-    Array.from(myMap.entries()).map((e) => {
-      const [key, value] = e;
-      value.forEach((v: any) => {
-        totalCount += v.count;
-      });
-    });
-    return totalCount;
-  };
-
-  const handleFolderAdd = (chip: StandardChip) => {
-    const megaChip = 5;
-    const gigaChip = 1;
-
-    if (myMap.has(chip.name)) {
-      const foundChips = myMap.get(chip.name);
-
-      const chipIndex = foundChips.findIndex(
-        (fC: any) => fC.lettercode === chip.lettercode
-      );
-
-      if (chipIndex > -1) {
-        const foundChip = foundChips[chipIndex];
-        const updatedChip = { ...chip, count: foundChip.count + 1 };
-        const clonedChips = [...foundChips];
-        clonedChips[chipIndex] = updatedChip;
-
-        setMap(new Map(myMap.set(chip.name, clonedChips)));
+  const handleFolderAdd2 = (chip: StandardChip) => {
+    const chipIndex = folderTrack.findIndex(
+      (c: any) => c.name.toLowerCase() === chip.name.toLowerCase()
+    );
+    if (chipIndex > -1) {
+      const currentChip = folderTrack.find((c) => c.name === chip.name);
+      if (currentChip.count === 4) {
+        alert("Can only have 4 of each chip");
       } else {
-        setMap(
-          new Map(
-            myMap.set(chip.name, [
-              ...foundChips,
-              {
-                count: 1,
-                name: chip.name,
-                lettercode: chip.lettercode,
-              },
-            ])
-          )
-        );
+        const chipData = folderTrack[chipIndex];
+        const updatedChip = { ...chipData, count: chipData.count + 1 };
+        const clonedFolderTrack = [...folderTrack];
+        clonedFolderTrack[chipIndex] = updatedChip;
+        setFolder2(clonedFolderTrack);
+        setFolder([...folder, chip]);
       }
     } else {
-      setMap(
-        new Map(
-          myMap.set(chip.name, [
-            { count: 1, name: chip.name, lettercode: chip.lettercode },
-          ])
-        )
-      );
+      setFolder2([...folderTrack, { count: 1, name: chip.name }]);
+      setFolder([...folder, chip]);
+    }
+  };
+
+  const removeChip = (chip: StandardChip, index: number) => {
+    const chipIndex = folderTrack.findIndex(
+      (c: any) => c.name.toLowerCase() === chip.name.toLowerCase()
+    );
+
+    if (chipIndex > -1) {
+      // const currentChip = folderTrack.find((c) => c.name === chip.name);
+      const chipData = folderTrack[chipIndex];
+      const updatedChip = { ...chipData, count: chipData.count - 1 };
+      console.log(updatedChip);
+      if (updatedChip.count === 0) {
+        console.log("hit");
+        setFolder2([
+          ...folderTrack.slice(0, chipIndex),
+          ...folderTrack.slice(chipIndex + 1),
+        ]);
+
+        setFolder([...folder.slice(0, index), ...folder.slice(index + 1)]);
+      } else {
+        const clonedFolderTrack = [...folderTrack];
+        clonedFolderTrack[chipIndex] = updatedChip;
+
+        setFolder2(clonedFolderTrack);
+        // https://stackoverflow.com/a/69458984
+        setFolder([...folder.slice(0, index), ...folder.slice(index + 1)]);
+      }
+    } else {
+      setFolder2([...folderTrack, { count: 1, name: chip.name }]);
+      setFolder([...folder, chip]);
     }
   };
 
@@ -68,29 +67,23 @@ function App() {
       {/* <div className="mx-4 max-w-7xl sm:mx-32"> */}
       <div className="grid grid-cols-1 gap-8 py-6 lg:grid-cols-2">
         <div className="pb-10 lg:w-5/6">
-          <p>Total Chips: {getTotalCount()} / 30</p>
+          <p>Total Chips: {folder.length} / 30</p>
 
-          {[...myMap.values()].map((e) => {
-            return e.map((p: any) => {
-              return (
-                <div className="" key={p.name + p.lettercode}>
-                  <div className="flex">
-                    <p>{p.name} </p> <p>{p.lettercode}</p>
-                  </div>
-                  <div className="flex">
-                    <p>Total: {p.count} / 4</p>
-                  </div>
-                  <button
-                    // onClick={() =>
-                    //   setFolderItem(folder.filter((c) => c.key !== chip.key))
-                    // }
-                    className="relative inline-flex items-center rounded-md border border-pink-700 bg-pink-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:border-pink-800 hover:bg-pink-700"
-                  >
-                    -
-                  </button>
-                </div>
-              );
-            });
+          {folder.map((chip, index) => {
+            return (
+              <div className="flex" key={chip.key}>
+                <span className="flex">
+                  {chip.name} {chip.lettercode}
+                </span>
+                <p>{chip.memory}</p>
+                <button
+                  onClick={() => removeChip(chip, index)}
+                  className="relative inline-flex items-center rounded-md border border-pink-700 bg-pink-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:border-pink-800 hover:bg-pink-700"
+                >
+                  -
+                </button>
+              </div>
+            );
           })}
         </div>
 
@@ -105,7 +98,7 @@ function App() {
                 <p>{chip.memory}</p>
                 <button
                   // onClick={() => setFolderItem([...folder, chip])}
-                  onClick={() => handleFolderAdd(chip)}
+                  onClick={() => handleFolderAdd2(chip)}
                   className="relative inline-flex items-center rounded-md border border-pink-700 bg-pink-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:border-pink-800 hover:bg-pink-700"
                 >
                   +
