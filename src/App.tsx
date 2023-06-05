a281ad615d80303556db94418a17acdc53226080
vchho <12765624+vchho@users.noreply.github.com>
2023-06-04 20:31:11 -0700

    UI Changes

--- src/App.tsx
+++ src/App.tsx
@@ -63,19 +63,19 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col items-center">
-      {/* <div className="mx-4 max-w-7xl sm:mx-32"> */}
-      <div className="grid grid-cols-1 gap-8 py-6 lg:grid-cols-2">
-        <div className="pb-10 lg:w-5/6">
+    <>
+      <div className="grid h-screen grid-cols-12 bg-blue-500">
+        <div className="col-span-6">
           <p>Total Chips: {folder.length} / 30</p>
 
-          {folder.map((chip, index) => {
+          {folderTrack.map((chip, index) => {
             return (
-              <div className="flex" key={chip.key}>
-                <span className="flex">
-                  {chip.name} {chip.lettercode}
-                </span>
-                <p>{chip.memory}</p>
+              <div
+                className="max-w mb-2 block h-24 rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100"
+                key={index}
+              >
+                <span className="flex">{chip.name}</span>
+                <p>{chip.count}</p>
                 <button
                   onClick={() => removeChip(chip, index)}
                   className="relative inline-flex items-center rounded-md border border-pink-700 bg-pink-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:border-pink-800 hover:bg-pink-700"
@@ -87,29 +87,118 @@ function App() {
           })}
         </div>
 
-        <div className="pb-10 lg:w-5/6">
-          <p>Standard</p>
-          {updatedStandardChips.map((chip) => {
-            return (
-              <div className="flex" key={chip.key}>
-                <span className="flex">
-                  {chip.name} {chip.lettercode}
-                </span>
-                <p>{chip.memory}</p>
-                <button
-                  // onClick={() => setFolderItem([...folder, chip])}
-                  onClick={() => handleFolderAdd2(chip)}
-                  className="relative inline-flex items-center rounded-md border border-pink-700 bg-pink-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:border-pink-800 hover:bg-pink-700"
-                >
-                  +
-                </button>
-              </div>
-            );
-          })}
+        <div className="col-span-6 flex flex-1 overflow-hidden">
+          <div className="flex-1 overflow-y-scroll">
+            <div className=" bg-lime-400">
+              <p>Standard</p>
+              {updatedStandardChips.map((chip) => {
+                return (
+                  <div
+                    className="max-w mb-2 block h-24 rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100"
+                    key={chip.key}
+                  >
+                    <div className="flex">
+                      {chip.name} {chip.lettercode}
+                    </div>
+                    <span className="flex">{chip.memory}</span>
+                    <button
+                      // onClick={() => setFolderItem([...folder, chip])}
+                      onClick={() => handleFolderAdd2(chip)}
+                      className="relative inline-flex items-center rounded-md border border-pink-700 bg-pink-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:border-pink-800 hover:bg-pink-700"
+                    >
+                      +
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
       {/* </div> */}
-    </div>
+      {/* </div> */}
+      <div className="fixed bottom-0 left-0 z-50 h-16 w-full border-t border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-700">
+        <div className="mx-auto grid h-full max-w-lg grid-cols-4 font-medium">
+          <button
+            type="button"
+            className="group inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
+          >
+            <svg
+              className="mb-1 h-6 w-6 text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+            </svg>
+            <span className="text-sm text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500">
+              All: {folder.length} / 30
+            </span>
+          </button>
+          <button
+            type="button"
+            className="group inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
+          >
+            <svg
+              className="mb-1 h-6 w-6 text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"></path>
+              <path
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
+              ></path>
+            </svg>
+            <span className="text-sm text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500">
+              Wallet
+            </span>
+          </button>
+          <button
+            type="button"
+            className="group inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
+          >
+            <svg
+              className="mb-1 h-6 w-6 text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"></path>
+            </svg>
+            <span className="text-sm text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500">
+              Settings
+            </span>
+          </button>
+          <button
+            type="button"
+            className="group inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800"
+          >
+            <svg
+              className="mb-1 h-6 w-6 text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                clip-rule="evenodd"
+                fill-rule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+              ></path>
+            </svg>
+            <span className="text-sm text-gray-500 group-hover:text-blue-600 dark:text-gray-400 dark:group-hover:text-blue-500">
+              Profile
+            </span>
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
