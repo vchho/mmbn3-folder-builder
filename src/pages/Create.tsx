68b5e636f3d66979411915c281ad952d3101bfa1
vchho <12765624+vchho@users.noreply.github.com>
2023-07-03 17:28:59 -0700

    updated create.tsx

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,13 +1,88 @@
 import { Chip } from "../utils/chips";
-import { useState, ChangeEvent, memo, Fragment } from "react";
-import { Menu, Tab, Transition } from "@headlessui/react";
+import { useState, ChangeEvent, memo, Fragment, useEffect } from "react";
+import { Menu, Tab, Transition, Listbox } from "@headlessui/react";
 import useBattleChips from "../hooks/useBattleChips";
 import { FolderNav } from "../components/FolderNav";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import {
+  ChevronDownIcon,
+  CheckIcon,
+  ChevronUpDownIcon,
+} from "@heroicons/react/20/solid";
 import useLocalStorage from "../hooks/useLocalStorage";
 import { nanoid } from "nanoid";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 // import Toggle from "./components/Toggle";
+
+type FolderParams = {
+  id: string;
+};
+
+const people = [
+  { id: 1, name: "Durward Reynolds", unavailable: false },
+  { id: 2, name: "Kenton Towne", unavailable: false },
+  { id: 3, name: "Therese Wunsch", unavailable: false },
+  { id: 4, name: "Benedict Kessler", unavailable: true },
+  { id: 5, name: "Katelyn Rohan", unavailable: false },
+];
+
+function MyListbox() {
+  const [selected, setSelected] = useState(people[0]);
+
+  return (
+    <div className="w-96">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <span className="block truncate">{selected.name}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {people.map((person, personIdx) => (
+                <Listbox.Option
+                  key={personIdx}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? "bg-amber-100 text-amber-900" : "text-gray-900"
+                    }`
+                  }
+                  value={person}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {person.name}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+}
 
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(" ");
@@ -64,15 +139,15 @@ const ChipItem = memo(function ChipItem({
   return (
     <div
       className={classNames(
-        "rounded-xl bg-white p-1",
+        "rounded-xl bg-zinc-100 p-1",
         "mb-3 ml-3 mr-3 ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
       )}
       key={chipIndex}
     >
       <div className="relative rounded-sm p-2">
         <div className="flex justify-between">
-          <div className="flex-row">
-            <div className="text-sm font-medium leading-6 text-gray-900">
+          <div className="flex-row sm:flex-col">
+            <div className="flex flex-col text-sm font-medium leading-6 text-gray-900">
               <label htmlFor="comments" className="font-medium text-gray-900">
                 {chip.name} {chip.lettercode}
               </label>
@@ -89,7 +164,7 @@ const ChipItem = memo(function ChipItem({
             />
             <button
               onClick={() => addChipToFolder(chip)}
-              className="h-11 self-center rounded-md border border-pink-700 bg-pink-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:border-pink-800 hover:bg-pink-700"
+              className="h-11 self-center rounded-md border border-green-700 bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:border-green-800 hover:bg-green-700"
             >
               +
             </button>
@@ -184,6 +259,8 @@ const sortOptions = [
 ];
 
 function Create() {
+  // Get folder id from URL
+  const { id } = useParams<FolderParams>();
   const [folder, setFolder] = useState<any[]>([]);
   const [folderTrack, setFolder2] = useState<FolderTrack[]>([]);
   const [currentTabIndex, setCurrentTabIndex] = useState(0);
@@ -197,10 +274,23 @@ function Create() {
   const [sortOptions2] = useState(sortOptions);
   const navigate = useNavigate();
 
-  const [value, setValue] = useLocalStorage<setFolder[]>(
+  const [storedValue, setValue] = useLocalStorage<setFolder[]>(
     "mmbn3-folder-builder",
     []
   );
+
+  console.log("stored Value", storedValue);
+
+  useEffect(() => {
+    if (id) {
+      const foundFolder = storedValue!.find((folder) => folder.id === id);
+      if (foundFolder) {
+        setFolder(foundFolder?.folder);
+        setFolder2(foundFolder?.folderTrack);
+      }
+    }
+    // When I get that value to come back, this useEffect will update
+  }, [storedValue]);
 
   const totalCount = folderTrack.reduce((accumulator, currentValue) => {
     return accumulator + currentValue.count;
@@ -352,14 +442,17 @@ function Create() {
   };
 
   const saveFolder = () => {
-    const updatedFolder = [...value, { folder, folderTrack, id: nanoid() }];
+    const updatedFolder = [
+      ...storedValue,
+      { folder, folderTrack, id: nanoid() },
+    ];
     setValue(updatedFolder);
     navigate("/");
   };
 
   return (
     <>
-      <div className="grid h-screen grid-cols-12 bg-blue-500">
+      <div className="grid h-screen grid-cols-12 bg-blue-300">
         <div className="col-span-6 flex-1 overflow-y-scroll">
           <header className="sticky top-0 z-50">
             <FolderNav
@@ -386,24 +479,27 @@ function Create() {
 
         <div className="col-span-6 flex flex-1 overflow-hidden">
           <div className="flex-1 overflow-y-scroll">
-            <div className="bg-lime-400">
+            <div className="bg-blue-300">
               <header className="sticky top-0 z-50">
                 <div className="mb-6">
                   <nav className="h-16 border-gray-200 bg-white dark:bg-gray-900">
-                    <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+                    <div className="mx-auto flex flex-wrap items-center justify-between p-4">
                       <SearchBox
                         handleChipSearch={handleChipSearch}
                         searchTerm={searchTerm}
                       />
+
+                      <MyListbox />
+
                       <Menu
                         as="div"
                         className="relative inline-block text-left"
                       >
-                        <div>
-                          <Menu.Button className="group inline-flex justify-center text-sm font-medium  text-white hover:text-gray-900 dark:hover:text-red-500">
+                        <div className="dark:hover:text-red-500">
+                          <Menu.Button className="group inline-flex justify-center text-sm font-medium  text-white">
                             Sort
                             <ChevronDownIcon
-                              className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 dark:hover:text-red-500"
+                              className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 "
                               aria-hidden="true"
                             />
                           </Menu.Button>
