114dd47b532efb595a75af7c1b33b065034d56ed
vchho <12765624+vchho@users.noreply.github.com>
2023-07-13 00:41:36 -0700

    Updated NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
   return (
-    <>
-      <div className="flex h-screen items-center justify-center">
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1>Let's go back</h1>
+      <Link to={"/"}>
         <img src="https://cdn.discordapp.com/attachments/210560171649269760/489644484850876446/17-MMBN3Update6146.png" />
-      </div>
-    </>
+      </Link>
+    </div>
   );
 };
